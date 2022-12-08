3c359a2b60e14b555efa2f40005ec37516dfa24d
Azeem Yaseen <azeemdomore@gmail.com>
2022-12-08 12:49:27 +0500

    updates on frontend

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,20 +2,29 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
+    debugScreens: {
+      position: ["bottom", "right"],
+    },
     extend: {
       colors: {
-        primary: '#000073',
-        secondary: '#FF006D',
+        primary: "#000073",
+        secondary: "#FF006D",
         light: {
-          gray: '#c4c4c4',
-          'lighter-gray': '#ECECEC'
-        }
+          gray: "#c4c4c4",
+          "lighter-gray": "#ECECEC",
+          purple: "#7878e833",
+          slate: "#E5E5E5",
+        },
+        dark: {
+          gray: "#1E1E1E",
+          purple: "#3636A5",
+        },
       },
       fontFamily: {
         inter: ["Inter", "sans-serif"],
         ebGaramond: ["EB Garamond", "serif"],
-        alef: ['Alef', 'sans-serif']
-      }
+        alef: ["Alef", "sans-serif"],
+      },
     },
   },
   plugins: [
